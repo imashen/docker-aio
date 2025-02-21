cd33c5803cf2971b030825fec7a8524752d4456f
Hutu <50132994+imashen@users.noreply.github.com>
2025-02-21 22:40:29 +0800

    Update workers.js

--- workers.js
+++ workers.js
@@ -1,16 +1,288 @@
-addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
-})
+let hub_host = 'registry-1.docker.io';
+const auth_url = 'https://auth.docker.io';
+let UA_Deny = ['netcraft'];
 
-async function handleRequest(request) {
-  const url = 'https://get.docker.com'
+function routeByHosts(host) {
+    const routes = {
+        "quay": "quay.io",
+        "gcr": "gcr.io",
+        "k8s-gcr": "k8s.gcr.io",
+        "k8s": "registry.k8s.io",
+        "ghcr": "ghcr.io",
+        "cloudsmith": "docker.cloudsmith.io",
+        "nvcr": "nvcr.io",
+        "test": "registry-1.docker.io",
+    };
+    if (host in routes) return [routes[host], false];
+    else return [hub_host, true];
+}
 
-  const response = await fetch(url)
-  const scriptContent = await response.text()
+const PREFLIGHT_INIT = {
+    headers: new Headers({
+        'access-control-allow-origin': '*',
+        'access-control-allow-methods': 'GET,POST,PUT,PATCH,TRACE,DELETE,HEAD,OPTIONS',
+        'access-control-max-age': '1728000',
+    }),
+}
 
-  return new Response(scriptContent, {
-      headers: {
-          'Content-Type': 'text/plain',
-      },
-  })
+function makeRes(body, status = 200, headers = {}) {
+    headers['access-control-allow-origin'] = '*'
+    return new Response(body, { status, headers })
+}
+
+function newUrl(urlStr, base) {
+    try {
+        return new URL(urlStr, base);
+    } catch (err) {
+        return null
+    }
+}
+
+async function nginx() {
+    return `	<!DOCTYPE html>
+	<html>
+	<head>
+	<title>Welcome to nginx!</title>
+	<style>
+		body {
+			width: 35em;
+			margin: 0 auto;
+			font-family: Tahoma, Verdana, Arial, sans-serif;
+		}
+	</style>
+	</head>
+	<body>
+	<h1>Welcome to nginx!</h1>
+	<p>If you see this page, the nginx web server is successfully installed and
+	working. Further configuration is required.</p>
+	
+	<p>For online documentation and support please refer to
+	<a href="http://nginx.org/">nginx.org</a>.<br/>
+	Commercial support is available at
+	<a href="http://nginx.com/">nginx.com</a>.</p>
+	
+	<p><em>Thank you for using nginx.</em></p>
+	</body>
+	</html>`;
+}
+
+async function searchInterface() {
+    return `	<!DOCTYPE html>
+	<html>
+	<head>
+	<title>Welcome to nginx!</title>
+	<style>
+		body {
+			width: 35em;
+			margin: 0 auto;
+			font-family: Tahoma, Verdana, Arial, sans-serif;
+		}
+	</style>
+	</head>
+	<body>
+	<h1>Welcome to Proxy!</h1>
+	<p>If you see this page, the proxy web server is successfully
+	working.</p>
+	
+	<p><em>Thank you for using this site.</em></p>
+	</body>
+	</html>`;
+}
+
+export default {
+    async fetch(request, env, ctx) {
+        const getReqHeader = (key) => request.headers.get(key);
+        let url = new URL(request.url);
+        const pathname = url.pathname;
+
+        if (pathname.startsWith('/install')) {
+            const newPath = pathname.replace(/^\/install/, '');
+            const targetUrl = new URL(newPath + url.search, 'https://get.docker.com');
+            return fetch(new Request(targetUrl, request));
+        }
+
+        const userAgentHeader = request.headers.get('User-Agent');
+        const userAgent = userAgentHeader ? userAgentHeader.toLowerCase() : "null";
+        if (env.UA) UA_Deny = UA_Deny.concat(await ADD(env.UA));
+        const workers_url = `https://${url.hostname}`;
+
+        const ns = url.searchParams.get('ns');
+        const hostname = url.searchParams.get('hubhost') || url.hostname;
+        const hostTop = hostname.split('.')[0];
+
+        let checkHost;
+        if (ns) {
+            if (ns === 'docker.io') {
+                hub_host = 'registry-1.docker.io';
+            } else {
+                hub_host = ns;
+            }
+        } else {
+            checkHost = routeByHosts(hostTop);
+            hub_host = checkHost[0];
+        }
+
+        const fakePage = checkHost ? checkHost[1] : false;
+        url.hostname = hub_host;
+        const hubParams = ['/v1/search', '/v1/repositories'];
+        if (UA_Deny.some(fxxk => userAgent.includes(fxxk)) && UA_Deny.length > 0) {
+            return new Response(await nginx(), {
+                headers: { 'Content-Type': 'text/html; charset=UTF-8' },
+            });
+        } else if ((userAgent && userAgent.includes('mozilla')) || hubParams.some(param => url.pathname.includes(param))) {
+            if (url.pathname == '/') {
+                if (env.URL302) {
+                    return Response.redirect(env.URL302, 302);
+                } else if (env.URL) {
+                    if (env.URL.toLowerCase() == 'nginx') {
+                        return new Response(await nginx(), {
+                            headers: { 'Content-Type': 'text/html; charset=UTF-8' },
+                        });
+                    } else return fetch(new Request(env.URL, request));
+                } else {
+                    if (fakePage) return new Response(await searchInterface(), {
+                        headers: { 'Content-Type': 'text/html; charset=UTF-8' },
+                    });
+                }
+            } else {
+                if (fakePage) url.hostname = 'registry.hub.docker.com';
+                if (url.searchParams.get('q')?.includes('library/') && url.searchParams.get('q') != 'library/') {
+                    const search = url.searchParams.get('q');
+                    url.searchParams.set('q', search.replace('library/', ''));
+                }
+                const newRequest = new Request(url, request);
+                return fetch(newRequest);
+            }
+        }
+
+        if (!/%2F/.test(url.search) && /%3A/.test(url.toString())) {
+            let modifiedUrl = url.toString().replace(/%3A(?=.*?&)/, '%3Alibrary%2F');
+            url = new URL(modifiedUrl);
+        }
+
+        if (url.pathname.includes('/token')) {
+            let token_parameter = {
+                headers: {
+                    'Host': 'auth.docker.io',
+                    'User-Agent': getReqHeader("User-Agent"),
+                    'Accept': getReqHeader("Accept"),
+                    'Accept-Language': getReqHeader("Accept-Language"),
+                    'Accept-Encoding': getReqHeader("Accept-Encoding"),
+                    'Connection': 'keep-alive',
+                    'Cache-Control': 'max-age=0'
+                }
+            };
+            let token_url = auth_url + url.pathname + url.search;
+            return fetch(new Request(token_url, request), token_parameter);
+        }
+
+        if (hub_host == 'registry-1.docker.io' && /^\/v2\/[^/]+\/[^/]+\/[^/]+$/.test(url.pathname) && !/^\/v2\/library/.test(url.pathname)) {
+            url.pathname = '/v2/library/' + url.pathname.split('/v2/')[1];
+        }
+
+        let parameter = {
+            headers: {
+                'Host': hub_host,
+                'User-Agent': getReqHeader("User-Agent"),
+                'Accept': getReqHeader("Accept"),
+                'Accept-Language': getReqHeader("Accept-Language"),
+                'Accept-Encoding': getReqHeader("Accept-Encoding"),
+                'Connection': 'keep-alive',
+                'Cache-Control': 'max-age=0'
+            },
+            cacheTtl: 3600
+        };
+
+        if (request.headers.has("Authorization")) {
+            parameter.headers.Authorization = getReqHeader("Authorization");
+        }
+
+        if (request.headers.has("X-Amz-Content-Sha256")) {
+            parameter.headers['X-Amz-Content-Sha256'] = getReqHeader("X-Amz-Content-Sha256");
+        }
+
+        let original_response = await fetch(new Request(url, request), parameter);
+        let original_response_clone = original_response.clone();
+        let original_text = original_response_clone.body;
+        let response_headers = original_response.headers;
+        let new_response_headers = new Headers(response_headers);
+        let status = original_response.status;
+
+        if (new_response_headers.get("Www-Authenticate")) {
+            let auth = new_response_headers.get("Www-Authenticate");
+            let re = new RegExp(auth_url, 'g');
+            new_response_headers.set("Www-Authenticate", response_headers.get("Www-Authenticate").replace(re, workers_url));
+        }
+
+        if (new_response_headers.get("Location")) {
+            const location = new_response_headers.get("Location");
+            return httpHandler(request, location, hub_host);
+        }
+
+        let response = new Response(original_text, {
+            status,
+            headers: new_response_headers
+        });
+        return response;
+    }
+};
+
+function httpHandler(req, pathname, baseHost) {
+    const reqHdrRaw = req.headers;
+    if (req.method === 'OPTIONS' &&
+        reqHdrRaw.has('access-control-request-headers')
+    ) {
+        return new Response(null, PREFLIGHT_INIT);
+    }
+
+    let rawLen = '';
+    const reqHdrNew = new Headers(reqHdrRaw);
+    reqHdrNew.delete("Authorization");
+
+    const urlObj = newUrl(pathname, 'https://' + baseHost);
+    const reqInit = {
+        method: req.method,
+        headers: reqHdrNew,
+        redirect: 'follow',
+        body: req.body
+    };
+    return proxy(urlObj, reqInit, rawLen);
+}
+
+async function proxy(urlObj, reqInit, rawLen) {
+    const res = await fetch(urlObj.href, reqInit);
+    const resHdrOld = res.headers;
+    const resHdrNew = new Headers(resHdrOld);
+
+    if (rawLen) {
+        const newLen = resHdrOld.get('content-length') || '';
+        const badLen = (rawLen !== newLen);
+        if (badLen) {
+            return makeRes(res.body, 400, {
+                '--error': `bad len: ${newLen}, except: ${rawLen}`,
+                'access-control-expose-headers': '--error',
+            });
+        }
+    }
+    const status = res.status;
+    resHdrNew.set('access-control-expose-headers', '*');
+    resHdrNew.set('access-control-allow-origin', '*');
+    resHdrNew.set('Cache-Control', 'max-age=1500');
+
+    resHdrNew.delete('content-security-policy');
+    resHdrNew.delete('content-security-policy-report-only');
+    resHdrNew.delete('clear-site-data');
+
+    return new Response(res.body, {
+        status,
+        headers: resHdrNew
+    });
+}
+
+async function ADD(envadd) {
+    var addtext = envadd.replace(/[    |"'\r\n]+/g, ',').replace(/,+/g, ',');
+    if (addtext.charAt(0) == ',') addtext = addtext.slice(1);
+    if (addtext.charAt(addtext.length - 1) == ',') addtext = addtext.slice(0, addtext.length - 1);
+    const add = addtext.split(',');
+    return add;
 }
